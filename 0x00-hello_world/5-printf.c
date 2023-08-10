842c101f3b4b749673a9a2d6d5961df4c99d6ee1
Osikucsudi <sudicollins007@gmail.com>
2023-08-10 13:13:24 -0700

    task 5

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - proper grammar
+ *
+ * Return: Should be 0
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
