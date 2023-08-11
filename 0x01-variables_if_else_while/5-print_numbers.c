3b1c6a63dbba8d7ae62533cfbbb74859dfe160f4
Osikucsudi <sudicollins007@gmail.com>
2023-08-11 11:23:43 -0700

    5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - base 10 digits
+ *
+ * Return: should be 0
+ */
+
+int main(void)
+{
+	int k;
+
+	for (k = 0 ; k < 10 ; k++)
+		printf("%d", k);
+	printf("\n");
+	return (0);
+}
+
