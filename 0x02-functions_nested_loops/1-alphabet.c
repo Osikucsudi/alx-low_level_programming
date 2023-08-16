262ff5c3249278fa3bcefbf2e4a85eab8de3ad72
Osikucsudi <sudicollins007@gmail.com>
2023-08-15 18:02:41 -0700

    task 1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * main - alphabet a-z
+ *
+ * Return: 0 r
+ */
+void print_alphabet(void)
+{
+	char k;
+
+	for (k = 'a' ; k <= 'z' ; k++)
+		_putchar(k);
+	_putchar('\n');
+}
