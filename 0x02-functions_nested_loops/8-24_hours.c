c142fa843de935f6b0621bda275f7196e0730326
Osikucsudi <sudicollins007@gmail.com>
2023-08-15 21:30:07 -0700

    task 8

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * jack_bauer - hours and minutes
+ * Return: hours and minutes
+ */
+
+void jack_bauer(void)
+{
+	int m, n;
+
+	for (m = 0 ; m < 23 ; m++)
+	{
+		for (n = 0 ; n < 60 ; n++)
+		{
+			_putchar(m / 10 + '0');
+			_putchar(m % 10 + '0');
+			_putchar(':');
+			_putchar(n / 10 + '0');
+			_putchar(n % 10 + '0');
+		}
+	}
+}
