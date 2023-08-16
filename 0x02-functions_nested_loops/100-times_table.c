f628a6dcb4ecd3a16444bf0852c5dec5171fc5a0
Osikucsudi <sudicollins007@gmail.com>
2023-08-16 03:00:51 -0700

    task 13

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(void)
+{
+	int sum = 0;
+	int h;
+
+	for (h = 1 ; h <= 10 ; h++)
+	{
+		if (h % 3 == 0 || h % 5 == 0)
+			sum = sum + 1;
+	}
+	printf("%d\n", sum);
+}
