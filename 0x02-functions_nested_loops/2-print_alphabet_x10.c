5167930aedad0378ecafe0249dea9b6b75a2583f
Osikucsudi <sudicollins007@gmail.com>
2023-08-15 18:47:53 -0700

    task 2

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -10,10 +10,10 @@ void print_alphabet_x10(void)
 	int a;
 	char m;
 
-	for (a =  '0' ; a <= '9' ; a++)
+	for (a = '0' ; a <= '9' ; a++)
 	{
 		for (m = 'a' ; m <= 'z' ; m++)
 			_putchar(m);
+		_putchar('\n');
 	}
-	_putchar('\n');
 }
