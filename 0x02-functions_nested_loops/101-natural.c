2403147cc0a64fdfeb310480e08e04b9834fe22e
Osikucsudi <sudicollins007@gmail.com>
2023-08-16 03:17:44 -0700

    task 101

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -1,14 +1,19 @@
 #include <stdio.h>
 
+/**
+ * main - function
+ * Return: 0
+ */
+
 int main(void)
 {
-        int sum = 0;
-        int h;
+	int sum = 0;
+	int h;
 
-        for (h = 1 ; h <= 10 ; h++)
-        {
-                if (h % 3 == 0 || h % 5 == 0)
-                        sum = sum + 1;
-        }
-        printf("%d\n", sum);
-}             
+	for (h = 1 ; h < 10 ; h++)
+	{
+		if (h % 3 == 0 || h % 5 == 0)
+			sum = sum + 1;
+	}
+	printf("%d\n", sum);
+}
