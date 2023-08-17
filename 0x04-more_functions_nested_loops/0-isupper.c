8f572b3fe536f0377f24adbab1fc51e336dfcfe6
Osikucsudi <sudicollins007@gmail.com>
2023-08-17 11:38:09 -0700

    task 0

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _isupper - uppercase or not
+ * @a: checked parameter
+ * Return: 1 if uppercase, 0 if not
+ */
+
+int _isupper(int a)
+{
+	if (a >= 65 && a <= 90)
+	{
+	return (1);
+	}
+	return (0);
+}
