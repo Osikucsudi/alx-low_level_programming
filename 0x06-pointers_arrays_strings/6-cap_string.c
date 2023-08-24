038209f7692e9b0a57fad8eb9475de1216776e58
Osikucsudi <sudicollins007@gmail.com>
2023-08-24 13:06:41 -0700

    task 6

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,35 @@
+#include "main.h"
+
+/**
+ * cap_string - function capitalizes word strings
+ * @str: string
+ * Return: string of word
+ */
+
+char *cap_string(char *str)
+{
+	int q = 0;
+
+	while (str[q])
+	{
+		while (!(str[q] >= 'a' && str[q] <= 'z'))
+			q++;
+		if (str[q - 1] == ' ' ||
+			str[q - 1] == '\t' ||
+			str[q - 1] == '\n' ||
+			str[q - 1] == ',' ||
+			str[q - 1] == ';' ||
+			str[q - 1] == '.' ||
+			str[q - 1] == '!' ||
+			str[q - 1] == '?' ||
+			str[q - 1] == '"' ||
+			str[q - 1] == '(' ||
+			str[q - 1] == ')' ||
+			str[q - 1] == '{' ||
+			str[q - 1] == '}' ||
+			q == 0)
+			str[q] -= 32;
+			q++;
+	}
+	return (str);
+}
